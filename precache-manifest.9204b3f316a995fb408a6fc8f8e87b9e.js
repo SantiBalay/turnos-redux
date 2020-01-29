749ed55e4453b14d1ed4c6b44478e9e6f10c7e3e
Santiago <santiagobalay1997@hotmail.com>
2020-01-29 14:56:11 -0300

    Updates

---
+++ precache-manifest.9204b3f316a995fb408a6fc8f8e87b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ced52df22015056fefefb77078dc2e8",
+    "url": "/turnos-redux/index.html"
+  },
+  {
+    "revision": "a80a80c24b2bf46492e1",
+    "url": "/turnos-redux/static/css/main.1fe8cb4d.chunk.css"
+  },
+  {
+    "revision": "06a3208cdcf0b62ed0bf",
+    "url": "/turnos-redux/static/js/2.97a52223.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/turnos-redux/static/js/2.97a52223.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a80a80c24b2bf46492e1",
+    "url": "/turnos-redux/static/js/main.5eb14cc1.chunk.js"
+  },
+  {
+    "revision": "f3d0b10b91d1057c05c8",
+    "url": "/turnos-redux/static/js/runtime-main.a420049a.js"
+  }
+]);
